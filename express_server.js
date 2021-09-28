b0d4376215ee573ca65954e172924b2c69e34bc3
Chitra <chitravenkitraman@gmail.com>
2021-09-28 14:59:27 -0600

    created shorturl and redirected to the original page

--- express_server.js
+++ express_server.js
@@ -37,16 +37,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 
-shortURL = generateRandomString();
- app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL; // updating the database
+  console.log(req.body.longURL);  // Log the POST request body to the console
+  res.redirect(`/urls/:${shortURL}`);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+   const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
