b4a5a5b4ee290e71b4d03f062836ee1d140bfe33
Chitra <chitravenkitraman@gmail.com>
2021-09-28 12:18:27 -0600

    added another route to get new url form via urls_new template

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -25,6 +27,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index',templateVars);
  });
+
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
  app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
